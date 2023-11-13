f15d11d4270b3ffd66a5d1c1a67096fead4ba717
FacundoHernandez122 <facundohernandez122@gmail.com>
2023-11-13 16:37:26 -0300

    update

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,27 +3,34 @@ import "./MainPage.css";
 
 function MainPage() {
   return (
-    <div className="myContainer">
+    <div className="container myContainer">
       <img className="myImg mt-5 p-3" src="myimg.jpeg" alt="" />
 
-      <div className="myName">
+      <div className="myName p-3">
         <h1>Facundo Hernández Casas</h1>
         <p>Full Stack Web Developer</p>
       </div>
-      <div className="skills mt-4 p-3">
-        <img className="me-3 ms-3" src="aws.png" alt="" />
-        <img className="me-3" src="bootstrap.png" alt="" />
-        <img className="me-3" src="css.png" alt="" />
-        <img className="me-3" src="github.png" alt="" />
-        <img className="me-3" src="html.png" alt="" />
-        <img className="me-3" src="javascript.png" alt="" />
-        <img className="me-3" src="mongodb.png" alt="" />
-        <img className="me-3" src="mysql.png" alt="" />
-        <img className="me-3" src="node.png" alt="" />
-        <img className="me-3" src="react.png" alt="" />
-        <img className="me-3" src="redux.png" alt="" />
+      <div className="row text-center">
+        <div className="col skills mt-3 p-3">
+          <img className="me-3 ms-3" src="aws.png" alt="" />
+          <img className="me-3" src="bootstrap.png" alt="" />
+          <img className="me-3" src="css.png" alt="" />
 
-        <img className="me-3" src="vue.png" alt="" />
+          <img className="me-3" src="github.png" alt="" />
+          <div className="col">
+            <img className="me-3" src="html.png" alt="" />
+            <img className="me-3" src="javascript.png" alt="" />
+            <img className="me-3" src="mongodb.png" alt="" />
+
+            <img className="me-3" src="mysql.png" alt="" />
+          </div>
+          <div className="col">
+            <img className="me-3" src="node.png" alt="" />
+            <img className="me-3" src="react.png" alt="" />
+            <img className="me-3" src="redux.png" alt="" />
+            <img className="me-3" src="vue.png" alt="" />
+          </div>
+        </div>
       </div>
       <div>
         <img className="scroll mt-5 " src="scroll-down.gif" alt="" />

6ae7d1c0d92564b60224d4924f775b5bc87a0d09
FacundoHernandez122 <facundohernandez122@gmail.com>
2023-11-13 13:49:30 -0300

    update

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',  // Agrega esta línea para especificar la base de la aplicación
   plugins: [react()],
 })
